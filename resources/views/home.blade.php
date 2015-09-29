9eb6559b54feffc698cc735ed2cc6f6452a7e293
Sajib32 <Sajibnstu@gmail.com>
2015-09-29 19:58:13 +0600

    Home,About,Contact page done.

---
+++ resources/views/home.blade.php
@@ -0,0 +1,20 @@
+<html>
+<head>
+    <title>Home Page</title>
+
+    <link href='//fonts.googleapis.com/css?family=Lato:100' rel='stylesheet' type='text/css'>
+    
+    <link rel="stylesheet" type="text/css" href="{!! asset('css/bootstrap.min.css') !!}" >
+    <link rel="stylesheet" href="{!! asset('css/bootstrap-theme.min.css') !!}">
+    <script src="{!! asset('js/bootstrap.min.js') !!}"></script>
+
+</head>
+<body>
+<div class="container">
+    <div class="content">
+        <div class="title">Home Page</div>
+        <div class="quote">Our Home page!</div>
+    </div>
+</div>
+</body>
+</html>
