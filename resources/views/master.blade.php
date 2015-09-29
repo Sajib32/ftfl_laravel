ec4d5868c60ba3f267e7b473f720c14f5eb8c631
Sajib32 <Sajibnstu@gmail.com>
2015-09-29 21:09:50 +0600

    Tickets database creation commplete

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -18,6 +18,7 @@
     @include('shared.navbar');
     
     @yield('content');
+    
     <script src="//code.jquery.com/jquery-1.10.2.min.js"></script>
 <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script>
 
